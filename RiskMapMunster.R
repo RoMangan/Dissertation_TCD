80e315aa5a7d01c1f4915f9a027b0690b98325fa
RoMangan <154017174+RoMangan@users.noreply.github.com>
2025-05-02 11:25:19 +0100

    Update RiskMapMunster.R

--- RiskMapMunster.R
+++ RiskMapMunster.R
@@ -1,9 +1,16 @@
 #Risk Map Munster counties
+# This script was developed by Rosie Mangan
+# 
+# 1 School of Computer Science and Statistics at Trinity College Dublin, Ireland.
+
+# Script originated May 15, 2024
+# Last modified Feb 12, 2025
 
 # load libraries
 
 library(sf)# sf is a library to read geospatial information, like maps
-library(dplyr) # dplyr is a data manipulation library
+library(dplyr) # dplyr is a 
+data manipulation library
 library(stringr) #stingr is library to manipulate string type data
 library(tidyverse)
 library(sp)
